89719e2c12d4afe8805909f2e842ae8ff05a102f
David Tolnay <dtolnay@gmail.com>
2025-12-25 14:44:58 -0800

    Add 4-digit bcd

--- src/bcd.rs
+++ src/bcd.rs
@@ -1,3 +1,10 @@
+fn to_bcd4(abcd: u16) -> u32 {
+    let abcd = u32::from(abcd);
+    let ab_cd = abcd + (0x10000 - 100) * ((abcd * 0x147b) >> 19);
+    let a_b_c_d = ab_cd + (0x100 - 10) * (((ab_cd * 0x67) >> 10) & 0xf000f);
+    a_b_c_d
+}
+
 fn to_bcd8(abcdefgh: u32) -> u64 {
     // An optimization from Xiang JunBo.
     // Three steps BCD. Base 10000 -> base 100 -> base 10.
@@ -14,8 +21,6 @@ fn to_bcd8(abcdefgh: u32) -> u64 {
     a_b_c_d_e_f_g_h
 }
 
-const ZEROS: u64 = 0x30303030_30303030; // 0x30 == '0'
-
 pub fn u64toa_bcd(value: u64, f: &dyn Fn(&str)) {
     if value < 100 {
         let offset = usize::from(value < 10);
@@ -25,18 +30,27 @@ pub fn u64toa_bcd(value: u64, f: &dyn Fn(&str)) {
                     [value as usize * 2 + offset..(value as usize + 1) * 2],
             )
         });
-    } else if value < 100_000_000 {
-        let bcd = to_bcd8(value as u32);
+    } else if value < 10_000 {
+        let bcd = to_bcd4(value as u16);
         let leading_zeros = bcd.leading_zeros() as usize / 8;
-        let bytes = (bcd | ZEROS).to_be_bytes();
+        let bytes = (bcd | 0x30303030).to_be_bytes();
         f(unsafe { str::from_utf8_unchecked(&bytes[leading_zeros..]) });
+    } else if value < 100_000_000 {
+        let bcd_hi = to_bcd4((value / 10_000) as u16);
+        let leading_zeros = bcd_hi.leading_zeros() as usize / 8;
+        let bcd_lo = to_bcd4((value % 10_000) as u16);
+        let bytes = [
+            (bcd_hi | 0x30303030).to_be_bytes(),
+            (bcd_lo | 0x30303030).to_be_bytes(),
+        ];
+        f(unsafe { str::from_utf8_unchecked(&bytes.as_flattened()[leading_zeros..]) });
     } else if value < 10_000_000_000_000_000 {
         let bcd_hi = to_bcd8((value / 100_000_000) as u32);
         let leading_zeros = bcd_hi.leading_zeros() as usize / 8;
         let bcd_lo = to_bcd8((value % 100_000_000) as u32);
         let bytes = [
-            (bcd_hi | ZEROS).to_be_bytes(),
-            (bcd_lo | ZEROS).to_be_bytes(),
+            (bcd_hi | 0x30303030_30303030).to_be_bytes(),
+            (bcd_lo | 0x30303030_30303030).to_be_bytes(),
         ];
         f(unsafe { str::from_utf8_unchecked(&bytes.as_flattened()[leading_zeros..]) });
     } else {
