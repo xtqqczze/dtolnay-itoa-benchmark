196b6e6ff4cebae77339d89e95d3cb80eb6fad88
David Tolnay <dtolnay@gmail.com>
2025-12-25 10:11:43 -0800

    Ignore similar_names pedantic clippy lint
        warning: binding's name is too similar to existing binding
          --> src/zmij.rs:28:9
           |
        28 |     let abcd_efgh = abcdefgh + (0x100000000 - 10000) * ((abcdefgh * 0x68db8bb) >> 40);
           |         ^^^^^^^^^
           |
        note: existing binding defined here
          --> src/zmij.rs:20:12
           |
        20 | fn to_bcd8(abcdefgh: u64) -> u64 {
           |            ^^^^^^^^
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#similar_names
           = note: `-W clippy::similar-names` implied by `-W clippy::pedantic`
           = help: to override `-W clippy::pedantic` add `#[allow(clippy::similar_names)]`

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
     clippy::cast_ptr_alignment,
     clippy::cast_sign_loss,
     clippy::many_single_char_names,
+    clippy::similar_names,
     clippy::too_many_lines,
     clippy::unreadable_literal
 )]
